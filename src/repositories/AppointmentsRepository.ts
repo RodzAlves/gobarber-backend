5c7dea40d83de4b84d37dd13415312014cb4eed6
RodzAlves <rodrigoalvesbrasileiro@gmail.com>
2021-01-12 16:23:14 -0300

    Service of creating a user in the database.

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -5,10 +5,6 @@ import Appointment from '../models/Appointment';
 @EntityRepository(Appointment)
 class AppointmentsRepository extends Repository<Appointment> {
   public async findByDate(date: Date): Promise<Appointment | null> {
-    /*const findAppointment = this.appointments.find(appointment =>
-      isEqual(appointment.date, date),
-    );*/
-
     const findAppointment = await this.findOne({
       where: { date },
     });
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -6,7 +6,6 @@ import { getCustomRepository } from 'typeorm';
 
 const appointmentsRouter = Router();
 
-// Rota GET
 appointmentsRouter.get('/', async (request, response) => {
   const appointsmentsRepository = getCustomRepository(AppointmentsReposiroty);
 
@@ -15,10 +14,9 @@ appointmentsRouter.get('/', async (request, response) => {
   return response.json(appointments);
 });
 
-// Rota POST
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
@@ -26,7 +24,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
 
     return response.json(appointment);
