104e709b98839a0d739123cfe1191096e92f6b2b
RodzAlves <rodrigoalvesbrasileiro@gmail.com>
2021-04-11 22:30:44 -0300

    fix: adjusment in middleware ensureAuth

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
 import authConfig from '../config/auth';
-import AppError from './../errors/AppError';
+import AppError from '../errors/AppError';
 
 interface TokenPayload {
   iat: number;
